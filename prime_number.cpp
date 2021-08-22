a650fca993c642fb1f8af5a06df6c9b950aca6fe
BengalDroid <bengaldroid@gmail.com>
2021-08-22 15:55:33 +0600

    function user define

--- prime_number.cpp
+++ prime_number.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std;
+bool isPrimeNumber(int number){
+bool isPrimeNumber =true;
+for(int i=2; i<number; i++){
+    if(number%i==0){
+        isPrimeNumber=false;
+        break;
+        
+    }
+    }
+    return isPrimeNumber;
+}
 int main() 
 {
     int number;
     cout <<"number is:"<<endl;
     cin>>number;
-    bool isPrimeNumber =true;
-    for(int i=2; i<number; i++){
-        if(number%i==0){
-            isPrimeNumber=false;
-            break;
-            
-        }
-    }
+    isPrimeNumber(number);
     if(isPrimeNumber){
         cout<<"prime number"<<endl;
     }else{
